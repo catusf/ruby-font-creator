276dd3f9555c7102c6d7161d3eaaecb054cb04ac
catus felis <phanmanhdan@gmail.com>
2024-11-14 02:35:56 +0000

    Fixes spaces

--- index.js
+++ index.js
@@ -2,32 +2,22 @@ import jsonfile from 'jsonfile'
 import webfont from 'webfont'
 import { argv } from 'yargs'
 
-
-
-
-
-
 import helpers from './src/helpers'
 import ruby from './src/ruby'
 import svg from './src/svg'
-
-
-
-
-
 
 function generateSvg(data, config) {
   const baseEngine = ruby.loadFont(config.baseFontFilepath)
   const rubyEngine = ruby.loadFont(config.rubyFontFilepath)
   const build_folder = `${config.workingDir}`
 
-  const fs = require('fs');
+  const fs = require('fs')
 
   if (!fs.existsSync(build_folder)) {
-    fs.mkdirSync(build_folder, { recursive: true });
-    console.log(`Folder created: ${build_folder}`);
+    fs.mkdirSync(build_folder, { recursive: true })
+    console.log(`Folder created: ${build_folder}`)
   } else {
-    console.log(`Folder already exists: ${build_folder}`);
+    console.log(`Folder already exists: ${build_folder}`)
   }
 
   for (let datum = 0; datum < data.length; datum += 1) {
