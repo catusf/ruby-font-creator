ab6963490cdaf9688e566891bbd9f8e97a8e34d0
Catus Phan <catus.phan@gmail.com>
2024-11-15 13:04:00 +0800

    Fixes change font

--- run_all.py
+++ run_all.py
@@ -8,29 +8,78 @@ from fontTools.ttLib import TTFont
 
 OUTPUT_DIR = 'output'
 
+def get_name_encoding(name):
+    """
+    Parameters:
+        names (NameRecord): Name record from the naming record
+    Returns:
+        The cmap codepoint encoding.
+        If GDI does not support the name, return None.
+    """
+    # From: https://github.com/MicrosoftDocs/typography-issues/issues/956#issuecomment-1205678068
+    if name.platformID == 3:
+        if name.platEncID == 3:
+            return "cp936"
+        elif name.platEncID == 4:
+            if name.nameID == 2:
+                return "utf_16_be"
+            else:
+                return "cp950"
+        elif name.platEncID == 5:
+            if name.nameID == 2:
+                return "utf_16_be"
+            else:
+                return "cp949"
+        else:
+            return "utf_16_be"
+    elif name.platformID == 1 and name.platEncID == 0:
+        return "iso-8859-1"
+
+    return None
+
+def get_decoded_name(name) -> str:
+    """
+    Parameters:
+        names (NameRecord): Name record from the naming record
+    Returns:
+        The decoded name
+    """
+
+    encoding = get_name_encoding(name)
+
+    if name.platformID == 3 and encoding != "utf_16_be":
+        # Compatibility for really old font
+        name_to_decode = name.string.replace(b"\x00", b"")
+    else:
+        name_to_decode = name.string
+
+    return name_to_decode.decode(encoding)
+
 def cleanup_font_name(input_file, output_file):
-    try:
-        # Load the input font
-        font = TTFont(input_file)
+    """ Remove the - character in font name
+    """
 
-        # Access the 'name' table which contains metadata like font name
-        name_table = font['name']
+    # Load the font file
+    font = TTFont(input_file)
 
-        # Iterate through all name entries and modify the family name (nameID == 1)
-        for record in name_table.names:
-            if record.nameID == 1:  # Font Family name
-                original_name = record.string.decode('utf-16-be')  # Decode to string
-                modified_name = original_name.replace("-", " ")  # Replace dashes with spaces
-                
-                print(f"Old: {original_name} New {modified_name}")
-                record.string = modified_name.encode('utf-16-be')  # Encode back to utf-16
+    fontFamilyName = font['name'].getDebugName(1)
+    newfontFamilyName = fontFamilyName.replace("-", " ")
 
-        # Save the modified font to the output file
-        font.save(output_file)
-        print(f"Font name changed successfully! Saved to {output_file}")
-    
-    except Exception as e:
-        print(f"Error: {e}")
+    # Access the 'name' table
+    name_table = font['name']
+
+    # Iterate through the name table to find the font name entry (name_id = 4)
+    for record in name_table.names:
+        encoding = get_name_encoding(record)
+
+        decoded_name = record.string.decode(encoding)
+        # print(f"{decoded_name=}")
+
+        if fontFamilyName == decoded_name:  # Font Name
+            record.string = newfontFamilyName.encode(encoding)
+
+    font.save(output_file)
+    print(f"Font has been updated and saved as '{newfontFamilyName}' in '{output_file}'.")
 
 def run_build_commands(data, config, save_config):
     # Generate the font name
@@ -87,7 +136,9 @@ def main(save_config=False, small_data=False):
         parameters = json.load(json_config)
         fontname = parameters['fontName']
         fontpath = os.path.join(OUTPUT_DIR, fontname+".ttf")
-        cleanup_font_name(fontpath, fontpath)
+        new_fontpath = os.path.join(OUTPUT_DIR, fontname+"-new.ttf")
+        
+        cleanup_font_name(fontpath, new_fontpath)
 
 import argparse
 
--- tools/change_font_name.py
+++ tools/change_font_name.py
@@ -1,29 +1,79 @@
 import sys
 from fontTools.ttLib import TTFont
 
-def change_font_name(input_file, output_file):
-    try:
-        # Load the input font
-        font = TTFont(input_file)
+def get_name_encoding(name):
+    """
+    Parameters:
+        names (NameRecord): Name record from the naming record
+    Returns:
+        The cmap codepoint encoding.
+        If GDI does not support the name, return None.
+    """
+    # From: https://github.com/MicrosoftDocs/typography-issues/issues/956#issuecomment-1205678068
+    if name.platformID == 3:
+        if name.platEncID == 3:
+            return "cp936"
+        elif name.platEncID == 4:
+            if name.nameID == 2:
+                return "utf_16_be"
+            else:
+                return "cp950"
+        elif name.platEncID == 5:
+            if name.nameID == 2:
+                return "utf_16_be"
+            else:
+                return "cp949"
+        else:
+            return "utf_16_be"
+    elif name.platformID == 1 and name.platEncID == 0:
+        return "iso-8859-1"
 
-        # Access the 'name' table which contains metadata like font name
-        name_table = font['name']
+    return None
 
-        # Iterate through all name entries and modify the family name (nameID == 1)
-        for record in name_table.names:
-            if record.nameID == 1:  # Font Family name
-                original_name = record.string.decode('utf-16-be')  # Decode to string
-                modified_name = original_name.replace("-", " ")  # Replace dashes with spaces
-                
-                print(f"Old: {original_name} New {modified_name}")
-                record.string = modified_name.encode('utf-16-be')  # Encode back to utf-16
+def get_decoded_name(name) -> str:
+    """
+    Parameters:
+        names (NameRecord): Name record from the naming record
+    Returns:
+        The decoded name
+    """
 
-        # Save the modified font to the output file
-        font.save(output_file)
-        print(f"Font name changed successfully! Saved to {output_file}")
-    
-    except Exception as e:
-        print(f"Error: {e}")
+    encoding = get_name_encoding(name)
+
+    if name.platformID == 3 and encoding != "utf_16_be":
+        # Compatibility for really old font
+        name_to_decode = name.string.replace(b"\x00", b"")
+    else:
+        name_to_decode = name.string
+
+    return name_to_decode.decode(encoding)
+
+def cleanup_font_name(input_file, output_file):
+    """ Remove the - character in font name
+    """
+
+    # Load the font file
+    font = TTFont(input_file)
+
+    fontFamilyName = font['name'].getDebugName(1)
+    newfontFamilyName = fontFamilyName.replace("-", " ")
+
+    # Access the 'name' table
+    name_table = font['name']
+
+    # Iterate through the name table to find the font name entry (name_id = 4)
+    for record in name_table.names:
+        encoding = get_name_encoding(record)
+
+        decoded_name = record.string.decode(encoding)
+        # print(f"{decoded_name=}")
+
+        if fontFamilyName == decoded_name:  # Font Name
+            record.string = newfontFamilyName.encode(encoding)
+
+    font.save(output_file)
+    print(f"Font has been updated and saved as '{newfontFamilyName}' in '{output_file}'.")
+
 
 if __name__ == "__main__":
     # Check if enough arguments were provided
@@ -33,4 +83,4 @@ if __name__ == "__main__":
         input_file = sys.argv[1]
         output_file = sys.argv[2]
 
-        change_font_name(input_file, output_file)
+        cleanup_font_name(input_file, output_file)
